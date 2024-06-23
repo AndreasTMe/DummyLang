3912f0c2e16672226a624a6b454e835967568bce
Andreas T. <atsimpan@gmail.com>
2024-06-23 13:11:00 +0100

    updated diagnostic messages

--- src/DummyLang.SyntacticAnalysis/Expressions/StringLiteralExpression.cs
+++ src/DummyLang.SyntacticAnalysis/Expressions/StringLiteralExpression.cs
@@ -5,6 +5,11 @@ namespace DummyLang.SyntacticAnalysis.Expressions;
 
 public sealed class StringLiteralExpression : Expression
 {
+    internal const string ShouldStartEndWithDoubleQuote =
+        "A string literal must start with a double quote (\") and end with a double quote (\").";
+    internal const string ShouldNotEscapeLastDoubleQuote =
+        "The last double quote of a string literal should not be escaped.";
+    
     public Token StringToken { get; }
 
     public StringLiteralExpression(Token stringToken)
--- src/DummyLang.SyntacticAnalysis/Expressions/UnaryExpression.cs
+++ src/DummyLang.SyntacticAnalysis/Expressions/UnaryExpression.cs
@@ -5,6 +5,8 @@ namespace DummyLang.SyntacticAnalysis.Expressions;
 
 public sealed class UnaryExpression : Expression
 {
+    internal const string AppliedToInvalidToken = "Invalid token next to a unary operator.";
+    
     public Token Token { get; }
     public Expression Expression { get; }
 
